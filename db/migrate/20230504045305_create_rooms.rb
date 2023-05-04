d9faea2ec27148ece987c4303d512a26a5b13ab9
takanashi11 <132178818+takanashi11@users.noreply.github.com>
2023-05-04 19:47:00 +0900

    プルダウンの実装

--- db/migrate/20230504045305_create_rooms.rb
+++ db/migrate/20230504045305_create_rooms.rb
@@ -1,8 +1,13 @@
 class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
-      t.string :neme, null: false
+      t.string :name, null: false
       t.timestamps
     end
   end
 end
+
+
+ 
+  
+ 
--- db/schema.rb
+++ db/schema.rb
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 2023_05_04_051356) do
   end
 
   create_table "rooms", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "neme", null: false
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
